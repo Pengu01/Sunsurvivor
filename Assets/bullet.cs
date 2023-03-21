9603a2ecb912429e2620e96dd1bdb458695520a3
Pengu <alfons.stoltz@gmail.com>
2023-03-21 13:55:52 +0100

    Animations

--- Assets/bullet.cs
+++ Assets/bullet.cs
@@ -7,6 +7,7 @@ public class bullet : MonoBehaviour
 {
     public Sprite sprite;
     public float speed;
+    public GameObject hit;
     float timer;
     void Update()
     {
@@ -14,5 +15,9 @@ public class bullet : MonoBehaviour
         timer += Time.deltaTime;
         if (timer > 0.25f) gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
         if (timer > 5.0f) Destroy(gameObject);
+    }
+    private void OnDestroy()
+    {
+        Instantiate(hit, transform.position, Quaternion.identity);
     }
 }
--- Assets/enemy.cs
+++ Assets/enemy.cs
@@ -6,20 +6,28 @@ using UnityEngine;
 public class enemy : MonoBehaviour
 {
     public float speed;
+    public GameObject explosion;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "bullet")
         {
-            Destroy(gameObject);
             Destroy(collision.gameObject);
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
         }
         if (collision.tag == "Player")
         {
+            Instantiate(explosion, collision.transform.position, Quaternion.identity);
             Destroy(collision.gameObject);
         }
     }
     private void Update()
     {
         transform.position = new Vector2(-speed*Time.deltaTime+transform.position.x, transform.position.y);
+        if (transform.position.y < -2) Destroy(gameObject);
+    }
+    private void OnDestroy()
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity);
     }
 }
