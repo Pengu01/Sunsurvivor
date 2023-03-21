2e98a3879a782cfd5912b622d630d6dedf7442f7
Pengu <alfons.stoltz@gmail.com>
2023-03-21 12:35:57 +0100

    Added bullets

---
+++ Assets/bullet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public Sprite sprite;
+    public float speed;
+    float timer;
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x + Time.deltaTime * speed, transform.position.y);
+        timer += Time.deltaTime;
+        if (timer > 0.25f) gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+        if (timer > 10.0f) Destroy(gameObject);
+    }
+}
