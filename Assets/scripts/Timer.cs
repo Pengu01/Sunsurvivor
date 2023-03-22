0e70e8833a0fda8ca8f94a5e8d17dbe6aa2a7763
Shark1n4Suit <sweshark83@gmail.com>
2023-03-22 19:58:45 +0100

    trying to fix shit github

--- Assets/scripts/Timer.cs
+++ Assets/scripts/Timer.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,12 +8,10 @@ public class Timer : MonoBehaviour
     float timer;
     Text text;
     int seconds;
-    GameObject highscore;
     // Start is called before the first frame update
     void Start()
     {
         text = gameObject.GetComponent<Text>();
-        highscore = GameObject.Find("Score");
     }
     private void Update()
     {
@@ -23,9 +20,7 @@ public class Timer : MonoBehaviour
         {
             timer -= 1.0f;
             seconds++;
-            string score = (Convert.ToInt32(highscore.GetComponent<Text>().text) + 5).ToString();
-            highscore.GetComponent<Text>().text = score;
-            if (seconds < 10)
+            if(seconds < 10)
             {
                 text.text = "0" + seconds.ToString();
             }
--- Assets/scripts/enemy.cs
+++ Assets/scripts/enemy.cs
@@ -17,6 +17,9 @@ public class enemy : MonoBehaviour
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+    }
+    private void Awake()
+    {
         highscore = GameObject.Find("Score");
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -28,8 +31,7 @@ public class enemy : MonoBehaviour
             gameObject.GetComponent<BoxCollider2D>().enabled = false;
             gameObject.GetComponent<SpriteRenderer>().enabled = false;
             audioSource.PlayOneShot(hit);
-            string score = (Convert.ToInt32(highscore.GetComponent<Text>().text) + 10).ToString();
-            highscore.GetComponent<Text>().text = score;
+            highscore.GetComponent<Text>().text = (Convert.ToInt32(highscore.GetComponent<Text>().text) + 10).ToString();
         }
         if (collision.tag == "Player")
         {
