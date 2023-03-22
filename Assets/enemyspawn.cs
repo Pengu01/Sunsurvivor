27cb3e33d0f63315db632d92cf16aca00dda2d2a
Pengu <alfons.stoltz@gmail.com>
2023-03-22 11:16:09 +0100

    Enemies spawn more randomly

--- Assets/enemyspawn.cs
+++ Assets/enemyspawn.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -8,6 +9,7 @@ public class enemyspawn : MonoBehaviour
     public GameObject enemy;
     public float enemyspawner;
     float enemyspawntimer;
+    int count;
     private void Start()
     {
         enemyspawntimer = enemyspawner;
@@ -19,9 +21,22 @@ public class enemyspawn : MonoBehaviour
         enemyspawntimer -= Time.deltaTime;
         if(enemyspawntimer < 0)
         {
+            if(UnityEngine.Random.Range(0, 4) == 2 && count == 0)
+            {
+                count = UnityEngine.Random.Range(1, 5);
+            }
             float randomfloat = UnityEngine.Random.Range(-659, 632);
             Instantiate(enemy, new Vector2(transform.position.x, randomfloat /1000),Quaternion.identity);
-            enemyspawntimer = enemyspawner;
+            float randomfloat2 = UnityEngine.Random.Range(89, 110);
+            if(count == 0)
+            {
+                enemyspawntimer = enemyspawner * randomfloat2 / 100;
+            }
+            else
+            {
+                enemyspawntimer = enemyspawner * randomfloat2 / 270;
+                count--;
+            }
         }
     }
 }
