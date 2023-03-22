df9fabd4a8306e6c73c76bd95eb0209eebc6dde3
Pengu <alfons.stoltz@gmail.com>
2023-03-22 14:24:25 +0100

    Updated darkness to be more fun :)

--- Assets/asteroids.cs
+++ Assets/asteroids.cs
@@ -15,11 +15,13 @@ public class asteroids : MonoBehaviour
     public float eventtimer;
     float starttimer = 10.0f;
     public GameObject asteroid;
-    string warning = "DANGER! ASTEROIDS!";
+    string warning = "ASTEROIDS! INBOUND!";
+    string warning2 = "DAMAGED! RADAR!";
     public float repeattimer;
     Text text;
     int count = 0;
     public bool asteroidss = false;
+    public bool darkness = false;
     public GameObject lights;
     SerializedProperty intense;
     private void Start()
@@ -35,21 +37,19 @@ public class asteroids : MonoBehaviour
         {
             eventtimer -= Time.deltaTime;
         }
-        if(eventtimer < 0)
+        if(eventtimer < 0 && lights.GetComponent<Light2D>().intensity == 0.5f)
         {
-            lights.GetComponent<Light2D>().intensity = 0.5f;
-            if (UnityEngine.Random.Range(0, 8) == 1 && !asteroidss)
+            if (UnityEngine.Random.Range(0, 6) == 1 && !asteroidss && !darkness)
             {
                 repeattimer = 1.0f;
                 asteroidss = true;
             }
-            eventtimer = eventrepeat;
-            if (UnityEngine.Random.Range(0, 8) == 1 && !asteroidss)
+            if (UnityEngine.Random.Range(0, 6) == 1 && !asteroidss && !darkness)
             {
-                eventtimer = UnityEngine.Random.Range(10, 20);
-                lights.GetComponent<Light2D>().intensity = 0.1f;
+                repeattimer = 1.0f;
+                darkness = true;
             }
-            eventtimer = eventrepeat;
+            else eventtimer = eventrepeat;
         }
         if(repeattimer < 0 && asteroidss)
         {
@@ -68,6 +68,30 @@ public class asteroids : MonoBehaviour
                 asteroidss = false;
                 eventtimer = eventrepeat;
             }
+        }
+        if(repeattimer < 0 && darkness)
+        {
+            if (text.text == warning2) text.text = "";
+            else text.text = warning2;
+            lights.GetComponent<Light2D>().intensity = lights.GetComponent<Light2D>().intensity - 0.1f;
+            if(count <= 4)
+            {
+                repeattimer = 0.5f;
+                count++;
+            }
+            else
+            {
+                count = 0;
+                darkness = false;
+                eventtimer = eventrepeat;
+                repeattimer = UnityEngine.Random.Range(10, 20);
+            }
+        }
+        if(!darkness && lights.GetComponent<Light2D>().intensity != 0.5f && repeattimer < 0)
+        {
+            lights.GetComponent<Light2D>().intensity = lights.GetComponent<Light2D>().intensity + 0.1f;
+            repeattimer = 0.5f;
+            eventtimer = eventrepeat;
         }
     }
 }
