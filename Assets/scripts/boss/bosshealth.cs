fead6b54d65f5480a9cba10312766ba0e87e80c4
Shark1n4Suit <sweshark83@gmail.com>
2023-03-22 20:20:16 +0100

    Revert "boss stage 2"
    This reverts commit f9d543073b51bb3a4849c16c6a353206649ccd1f.

--- Assets/scripts/boss/bosshealth.cs
+++ Assets/scripts/boss/bosshealth.cs
@@ -8,10 +8,6 @@ public class bosshealth : MonoBehaviour
     public float damageamount = 1;
     public float maxhealthfase1 = 25;
     public float currenthealth;
-    public GameObject weapon;
-    public GameObject weapon2;
-    public GameObject fase1;
-
 
     // Start is called before the first frame update
     void Start()
@@ -25,14 +21,6 @@ public class bosshealth : MonoBehaviour
         if (currenthealth <= 0)
         {
             Destroy(gameObject);
-        }
-
-        if (currenthealth < 15)
-        {
-            //enter stage 2
-            weapon.GetComponent<enemyshoot>().enabled = false;
-            weapon2.GetComponent<myshootscript2>().enabled = false;
-            fase1.GetComponent<bosschange>().enabled = true;
         }
     }
    
